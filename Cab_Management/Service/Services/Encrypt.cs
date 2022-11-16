db2476d1a8469bfd92bb31aefbd1766698c016bb
11vanshika <vanshikaa@moback.com>
2022-11-16 18:09:33 +0530

    Some Changes made in servies

--- Cab_Management/Service/Services/Encrypt.cs
+++ Cab_Management/Service/Services/Encrypt.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Service.Inteface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,20 +7,34 @@ using System.Threading.Tasks;
 
 namespace Service.Services
 {
-    public class Encrypt
+    public class Encrypt: IEncrypt
     {
 
-        public string Decrypt_Password(string encryptpassword)
+        public string EncodePasswordToBase64(string password)
         {
-            string pswstr = string.Empty;
-            System.Text.UTF8Encoding encode_psw = new System.Text.UTF8Encoding();
-            System.Text.Decoder Decode = encode_psw.GetDecoder();
-            byte[] todecode_byte = Convert.FromBase64String(encryptpassword);
-            int charCount = Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
-            char[] decoded_char = new char[charCount];
-            Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
-            pswstr = new String(decoded_char);
-            return pswstr;
+            try
+            {
+                byte[] encData_byte = new byte[password.Length];
+                encData_byte = System.Text.Encoding.UTF8.GetBytes(password);
+                string encodedData = Convert.ToBase64String(encData_byte);
+                return encodedData;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Encode" + ex.Message);
+            }
         }
+        public string Decrypt_Password(string encodedData)
+        {
+            System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+            System.Text.Decoder utf8Decode = encoder.GetDecoder();
+            byte[] todecode_byte = Convert.FromBase64String(encodedData);
+            int charCount = utf8Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
+            char[] decoded_char = new char[charCount];
+            utf8Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
+            string result = new String(decoded_char);
+            return result;
+        }
+
     }
 }
