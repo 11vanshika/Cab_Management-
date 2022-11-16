8987d10585aea42c6ba3b0609ae616abbf70f195
Avezz78 <mohammeda@moback.com>
2022-11-16 10:07:12 +0530

    worked on services

---
+++ Cab_Management/Service/Inteface/IEncrypt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Service.Inteface
+{
+    public interface IEncrypt
+    {
+        public string Decrypt_Password(TabUsersDetail tabUsersDetail);
+    }
+}
---
+++ Cab_Management/Service/Inteface/IUserDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Service.Inteface
+{
+    public interface IUserDetails
+    {
+        List<TabUsersDetail> GetUsersDetails();
+        public string Register(TabUsersDetail tabUsersDetail);
+
+        public bool Update(TabUsersDetail tabUsersDetail);  
+
+       public bool Delete(TabUsersDetail tabUsersDetail);
+
+    }
+}
