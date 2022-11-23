e807be57052916975843abf9ef1f6789568d466b
jyothivmatam <jyothiv@moback.com>
2022-11-23 15:49:52 +0530

    I have completed the Unit testing of First Module

--- Cab_Management/CabManagementServiceTests/Service/UserDetailServiceDuplicate.cs
+++ Cab_Management/CabManagementServiceTests/Service/UserDetailServiceDuplicate.cs
@@ -1,82 +1,97 @@
 ﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Service.Inteface;
+using Service.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Persistence;
+using System.Runtime.Intrinsics.X86;
 
 namespace CabManagementServiceTests.Service
 {
-    public class UserDetailServiceDuplicate
+    public class UserDetailServiceDuplicate : IUserDetails
     {
         private readonly List<TbUser> userList;
-        private readonly List<TbUser> userDisplay;
+        private readonly IEncrypt _encrypt;
         public UserDetailServiceDuplicate()
         {
+            _encrypt = new Encrypt();
+
             userList = new List<TbUser>()
              {
                 new TbUser() { UserId=1, FirstName="jyothi", LastName="V", EmailId="jyothi@gmail.com",  Password="1234", UserRoleId=1, CreateDate=DateTime.Now, UpdateDate=null, Status=1},
                 new TbUser() { UserId=2, FirstName="vanshika", LastName="Agarwal", EmailId="vanshika@gmail.com",  Password="1234", UserRoleId=1, CreateDate=DateTime.Now, UpdateDate=null,Status=1},
                 new TbUser() { UserId=3, FirstName="avezz", LastName="mohammed", EmailId="avezz@gmail.com",  Password="1234", UserRoleId=1, CreateDate=DateTime.Now, UpdateDate=null,Status=1},
             };
-            userDisplay = new List<TbUser>()
-            {
-                new TbUser() { UserId=1, FirstName="jyothi", LastName="V", EmailId="bvgokulgok@gmail.com",UserRoleId=1, CreateDate=DateTime.Now, UpdateDate=null, Status=1},
-                new TbUser() { UserId=2, FirstName="vanshika", LastName="Agarwal", EmailId="diptesh@gmail.com", UserRoleId=1, CreateDate=DateTime.Now, UpdateDate=null, Status=1},
-                new TbUser() { UserId=3, FirstName="avezz", LastName="mohammed", EmailId="kazeem@gmail.com",  UserRoleId=1, CreateDate=DateTime.Now, UpdateDate=null, Status=1},
-            };
         }
-    }
-        public List<TbUser> GetUser()
+
+
+        public List<TbUser> GetUsersDetails()
         {
-            return userDisplay.ToList();
+            return userList.ToList();
         }
-        public bool CheckExtistUser(Registration user)
+        public bool CheckExtistUser(TbUser tbluser)
         {
-            TblUser user1 = userList.Where(x => x.Email == user.Email).FirstOrDefault()!;
+            TbUser user1 = userList.Where(x => x.EmailId == tbluser.EmailId).FirstOrDefault()!;
             if (user1 is null)
             {
                 return false;
             }
             return true;
         }
-        public bool CheckPassword(Registration user)
+        public bool CheckConfirmPassword(Login tbluser)
         {
-            TblUser user1 = userList.Where(x => x.Email == user.Email).FirstOrDefault()!;
-            if (user.Password == user.ConfirmPassword)
+            TbUser user1 = userList.Where(x => x.EmailId == tbluser.EmailId).FirstOrDefault()!;
+            if (tbluser.Password == tbluser.ConfirmPassword)
             {
                 return true;
             }
             return false;
         }
-        public bool Registration(Registration user)
+        public bool CheckExtistUser(Login login)
         {
-            user.CreatedDate = DateTime.Now;
-            user.UpdatedDate = null;
-            user.Active = true;
-            userList.Add(user);
+            var Email = userList.Where(x => x.EmailId == login.EmailId).FirstOrDefault();
+            if (Email == null)
+            {
+                return false;
+            }
             return true;
         }
-        public bool LogIn(TblUser login)
+        public bool Register(TbUser tbluser)
         {
-            TblUser user = userList.Where(x => x.Email == login.Email && x.Password == login.Password!).FirstOrDefault()!;
+            tbluser.CreateDate = DateTime.Now;
+            tbluser.UpdateDate = null;
+            tbluser.Status = 1;
+            userList.Add(tbluser);
+            return true;
+        }
+        public bool UserLogin(Login login)
+        {
+            TbUser user = userList.Where(x => x.EmailId == login.EmailId && x.Password == _encrypt.EncodePasswordToBase64(login.Password)).FirstOrDefault();
+
             if (user != null)
             {
                 return true;
             }
             return false;
         }
-        public bool ForgetPassword(Registration changePassword)
+        public bool ForgotPassword(Login login)
         {
-            int index = userList.FindIndex(item => item.Email == changePassword.Email);
-            TblUser user1 = userList.Where(x => x.Email == changePassword.Email).FirstOrDefault()!;
-            user1.Password = changePassword.Password;
-            user1.UpdatedDate = DateTime.Now;
+            int index = userList.FindIndex(item => item.EmailId == login.EmailId);
+            TbUser user1 = userList.Where(x => x.EmailId == login.EmailId).FirstOrDefault()!;
+            user1.Password = _encrypt.EncodePasswordToBase64(login.Password);
+            user1.Password = login.Password;
+            user1.UpdateDate = DateTime.Now;
             userList[index] = user1;
             return true;
         }
     }
 }
 
-    }
-}
+
+
+
+
