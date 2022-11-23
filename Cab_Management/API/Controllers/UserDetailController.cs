1350ff37dcc5f46d72e1d9922bf035e38f3eb8e7
Avezz78 <mohammeda@moback.com>
2022-11-23 19:08:04 +0530

    removed old unit test

--- Cab_Management/API/Controllers/UserDetailController.cs
+++ Cab_Management/API/Controllers/UserDetailController.cs
@@ -18,6 +18,7 @@ namespace API.Controllers
     {
         private readonly DbCabServicesContext dbCabservice;
         private readonly IUserDetails IuserDetails;
+
         public UserDetailsController(DbCabServicesContext dbContext, IUserDetails iuserDetails)
         {
             dbCabservice = dbContext;
