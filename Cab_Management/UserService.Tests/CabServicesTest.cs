2e8c674a4a000858cdbe1c7e5458114d5b12d5c5
Avezz78 <mohammeda@moback.com>
2022-12-01 13:44:40 +0530

    completed 2nd module unit testing

---
+++ Cab_Management/UserService.Tests/CabServicesTest.cs
@@ -0,0 +1,171 @@
+using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Options;
+using Persistence;
+using Service.Services;
+using UserDetailService.Tests;
+
+namespace CabManagementTestProject
+{
+    [Collection("Database Collection")]
+    public class CabServicesTest
+    {
+       
+        private readonly DataBaseFixture dataBaseFixture;
+        private readonly CabDetailService service;
+       
+
+      public CabServicesTest(DataBaseFixture dataBaseFixture)
+        {
+            this.dataBaseFixture = dataBaseFixture;
+            service = new CabDetailService(dataBaseFixture.context);
+        }
+
+        [Fact]
+
+        public void Add_NewCab()
+        {
+           //Arrange
+            var cab = new TbCabDetail()
+            {
+
+                Cabid = 6,
+                RegistrationNun = "4569878",
+                CabTypeId = 1,
+                UpdateDate = null,
+                Status = 1,
+                CreateDate = DateTime.Now,
+                UserId = 15,
+                CabType = null,
+                User = null
+            };
+           
+            //Act
+            var result = service.AddCab(cab);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GetAll_CabDetails()
+        {
+            //Arrange
+            var result = service.GetTbCabDetails();
+            var count=dataBaseFixture.context.TbCabDetails.Count();
+            //Act
+            var items = Assert.IsType<List<TbCabDetail>>(result);
+            //Assert
+            Assert.Equal(count, items.Count);
+        }
+        [Fact]
+        public void RemoveCab()
+        {
+            //Arrange
+            var cab = new TbCabDetail()
+            {
+                Cabid = 9,
+                RegistrationNun = "657890",
+                CabTypeId = 1,
+                UpdateDate = null,
+                Status = 1,
+                CreateDate = DateTime.Now,
+                UserId = 15,
+                CabType = null,
+                User = null
+            };
+
+            //Act
+            var result = service.RemoveCab(cab);
+
+            //Assert
+            Assert.True(result);  
+        }
+
+        [Fact]
+
+        public void UpdateCab()
+        {
+           //Arrange
+            var updatecab = new TbCabDetail()
+            {
+                Cabid = 5,
+                RegistrationNun = "456987",
+                CabTypeId = 1,
+            };
+           
+          
+            //Act
+            var result = service.UpdateCab(updatecab);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+
+        public void CheckRegNum()
+        {
+           //Arrange
+            var RegNum = new TbCabDetail()
+            {
+                RegistrationNun = "456987",
+                CabTypeId = 2
+
+            };
+
+            //ACT
+            var result = service.CheckRegNum(RegNum);
+            //Assert
+            Assert.True(result);
+            
+        }
+        [Fact]
+        public void newCheckRegNum()
+        {
+            //Arrange
+            var regNum = new TbCabDetail()
+            {
+                RegistrationNun = "45698",
+                CabTypeId = 1
+
+            };
+            //Act
+            var result = service.CheckRegNum(regNum);
+            //Assert
+            Assert.False(result);
+           
+        }
+
+        [Fact]
+        public void CheckAdmin()
+        {
+            //Arrange
+            var Admin = new TbCabDetail()
+            {
+                UserId = 15,
+            };
+            //Act
+            var result = service.CheckAdmin(Admin);
+            //Assert
+            Assert.True(result);
+            
+        }
+
+        [Fact]
+        public void NotAdmin()
+        {
+            //Arrange
+            var Admin = new TbCabDetail()
+            {
+                UserId = 1,
+            };
+            //Act
+            var result = service.CheckAdmin(Admin);
+            //Assert
+            Assert.False(result);
+        }
+    }
+}
--- Cab_Management/UserService.Tests/DataBaseFixture.cs
+++ Cab_Management/UserService.Tests/DataBaseFixture.cs
@@ -27,12 +27,23 @@ namespace UserDetailService.Tests
         {
             var user = new List<TbUser>()
             {
-                new TbUser(){UserId = 22, FirstName = "jyothi",LastName = "matam",EmailId = "jyothi@gmail.com",Password = "12345",UserRoleId = 1,CreateDate = DateTime.Now,UpdateDate = null,Status = 1},
-                new TbUser(){UserId = 23, FirstName = "avez",LastName = "md",EmailId = "avz@gmail.com",Password = "123456",UserRoleId = 1,CreateDate = DateTime.Now,UpdateDate = null,Status = 1}
+                new TbUser(){UserId = 1, FirstName = "jyothi",LastName = "matam",EmailId = "jyothi@gmail.com",Password = "12345",UserRoleId = 1,CreateDate = DateTime.Now,UpdateDate = null,Status = 1},
+                new TbUser(){UserId = 15, FirstName = "avez",LastName = "md",EmailId = "avz@gmail.com",Password = "123456",UserRoleId = 2,CreateDate = DateTime.Now,UpdateDate = null,Status = 1}
             };
             context.TbUsers.AddRange(user);
             context.SaveChanges();
+            var tbCabDetails = new List<TbCabDetail>()
+            {
+
+                new TbCabDetail(){Cabid=5,RegistrationNun="456987",CabTypeId=2,UpdateDate=null,Status=1,CreateDate=DateTime.Now,UserId=15,CabType=null,User=null},
+                new TbCabDetail(){Cabid=9,RegistrationNun="657890",CabTypeId=1,UpdateDate=null,Status=1,CreateDate=DateTime.Now,UserId=15,CabType=null,User=null}
+
+            };
+            context.TbCabDetails.AddRange(tbCabDetails);
+            context.SaveChanges();
+
         }
+    
         public void Dispose()
         {
             context.Database.EnsureDeleted();
